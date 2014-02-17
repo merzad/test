98986786a949969e5f6dc1436d3afdb143962f63
merzad <merzad1@gmail.com>
2014-02-16 22:48:50 -0500

    Update setenv.bat

--- build/setenv.bat
+++ build/setenv.bat
@@ -1,7 +1,6 @@
-
 set BUILD_CP=%CLASSPATH%
 set BUILD_CP=%BUILD_CP%;%JAVA_HOME%\lib\tools.jar;
-set BUILD_CP=%BUILD_CP%;..\devlib\ant.jar;
-set BUILD_CP=%BUILD_CP%;..\devlib\ant-junit.jar;
-set BUILD_CP=%BUILD_CP%;..\devlib\ant-launcher.jar;
-set BUILD_CP=%BUILD_CP%;..\devlib\junit.jar;
+set BUILD_CP=%BUILD_CP%;..\Development\ant.jar;
+set BUILD_CP=%BUILD_CP%;..\Development\ant-junit.jar;
+set BUILD_CP=%BUILD_CP%;..\Development\ant-launcher.jar;
+set BUILD_CP=%BUILD_CP%;..\Development\junit.jar;
